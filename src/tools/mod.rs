56f25eaf5aa9430363b10cb6f48e0235c01df4ee
Mll <mllcms@163.com>
2024-03-01 21:23:23 +0800

    feat(mod): 增加 tools 工具模块

---
+++ src/tools/mod.rs
@@ -0,0 +1,30 @@
+mod macros;
+pub mod multipart;
+pub mod resp;
+pub mod validate;
+
+use std::net::{Ipv4Addr, SocketAddr};
+
+use color_string::{pcs, Colored, Font::*};
+
+pub fn prompt_address(addr: &SocketAddr, protocol: &str) {
+    let mut ips = vec![addr.ip()];
+    if addr.ip() == Ipv4Addr::new(0, 0, 0, 0) {
+        if let Ok(vec) = if_addrs::get_if_addrs() {
+            let (mut local, mut network): (Vec<_>, Vec<_>) = vec.into_iter().map(|m| m.ip()).partition(|p| p.is_ipv4());
+            local.sort();
+            network.sort();
+            local.extend(network);
+            ips = local;
+        }
+    }
+
+    let port = addr.port().bold();
+    for ip in ips.iter().filter(|f| f.is_ipv4()) {
+        if ip.is_loopback() {
+            pcs!(Green => "➜  "; RBold => "Local:   "; RCyan => format!("{protocol}://{ip}:{port}"));
+        } else {
+            pcs!(Green => "➜  "; RBold => "Network: "; RCyan => format!("{protocol}://{ip}:{port}"));
+        }
+    }
+}
