565eb4a83768384822300f452013c296f317117b
Mll <mllcms@163.com>
2024-03-05 05:55:31 +0800

    feat: 增加 jwt 配置

---
+++ src/config/jwt.rs
@@ -0,0 +1,23 @@
+use std::fmt::{Debug, Formatter};
+
+use serde::{Deserialize, Deserializer};
+
+use crate::middleware::jwt::Secret;
+
+#[derive(Clone, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct JwtConfig {
+    #[serde(deserialize_with = "parse_secret")]
+    pub secret: Secret,
+    pub duration: u64,
+}
+
+fn parse_secret<'de, D: Deserializer<'de>>(de: D) -> Result<Secret, D::Error> {
+    Ok(Secret::new(&String::deserialize(de)?))
+}
+
+impl Debug for JwtConfig {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Jwt").field("duration", &self.duration).finish()
+    }
+}
