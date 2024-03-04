c5bef5d612027b4abbce50b847ada7d666de7ace
Mll <mllcms@163.com>
2024-03-04 09:09:30 +0800

    refactor(mod): 使用宏简化构建 优化使用体验

--- src/tools/resp.rs
+++ src/tools/resp.rs
@@ -1,15 +1,21 @@
-use std::{fmt::Display, io};
+//! ```rust,ignore
+//! async fn demo() -> Resp<String> {
+//!     let data = /*handle*/.map_err(|err|res!(400,"{err}"))?;
+//!
+//!     if let Err(err) = /*handle*/ {
+//!        reject(400, "{err}")
+//!     }else {
+//!        resolve!(200 => data, "ok")
+//!     }
+//! }
+//! ```
 
-use axum::{
-    body::Body,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-};
-use axum_extra::extract::multipart::MultipartError;
+use std::fmt::Display;
+
+use axum::response::{IntoResponse, Response};
 use serde::Serialize;
 
-pub type Resp<T> = std::result::Result<Res<T>, Res<()>>;
-pub type Result<T> = std::result::Result<T, Res<()>>;
+pub type Resp<T> = Result<Res<T>, Res<()>>;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Res<T: Serialize = ()> {
@@ -18,77 +24,48 @@ pub struct Res<T: Serialize = ()> {
     pub data: T,
 }
 
-impl Res<()> {
-    pub fn msg(code: u16, msg: impl Display) -> Self {
-        Self::new(code, msg, ())
-    }
-    pub fn err(msg: impl Display) -> Self {
-        Self::new(400, msg, ())
-    }
-}
-
 impl<T: Serialize> Res<T> {
     pub fn new(code: u16, msg: impl Display, data: impl Into<T>) -> Self {
         Self { code, message: msg.to_string(), data: data.into() }
-    }
-
-    pub fn data(code: StatusCode, data: impl Into<T>) -> Self {
-        Self::new(code.as_u16(), code, data)
     }
 }
 
 impl<T: Serialize> IntoResponse for Res<T> {
     fn into_response(self) -> Response {
-        let body = serde_json::to_string(&self).unwrap();
         Response::builder()
             .status(self.code)
             .header("Content-type", "application/json")
-            .body(Body::new(body))
+            .body(serde_json::to_vec(&self).unwrap().into())
             .unwrap()
     }
 }
 
-macro_rules! res_from {
-    ($($t:ty, $c:expr $(;)?)*) => {
-        $(
-            impl From<$t> for Res<()> {
-                fn from(value: $t) -> Self {
-                    Res::new($c, value, ())
-                }
-            }
-        )*
-    };
-
-    ($($t:ty, $c:expr, $m:expr $(;)?)*) => {
-    $(
-        impl From<$t> for Res<()> {
-            fn from(_: $t) -> Self {
-                Res::new($c, $m, ())
-            }
-        }
-    )*
-    };
-}
-
-res_from!(
-    &str, 400
-    String, 400;
-    io::Error, 400;
-    anyhow::Error,400
-    MultipartError, 422
-    serde::de::value::Error, 422;
-);
-
-// res_from!(MultipartError, 422, "提取数据失败");
-
-impl From<StatusCode> for Res<()> {
-    fn from(value: StatusCode) -> Self {
-        Res::new(value.as_u16(), value.as_str(), ())
+impl<F: Display> From<F> for Res {
+    fn from(value: F) -> Self {
+        Self::new(400, value, ())
     }
 }
 
-impl<T: Serialize> From<(StatusCode, T)> for Res<T> {
-    fn from((code, data): (StatusCode, T)) -> Self {
-        Res::new(code.as_u16(), code.as_str(), data)
-    }
+#[macro_export]
+macro_rules! res {
+    ($code:expr, $($msg:tt)+) => {
+        Res::new($code, format!($($msg)+), ()) as Res
+    };
+    ($code:expr => $data:expr, $($msg:tt)+) => {
+        Res::new($code, format!($($msg)+), $data)
+    };
+}
+
+#[macro_export]
+macro_rules! reject {
+    ($($t:tt)*) => {
+        Err($crate::res!($($t)*))
+    };
+}
+
+#[macro_export]
+macro_rules! resolve {
+    ($($t:tt)*) => {
+        Ok($crate::res!($($t)*))
+    };
 }
